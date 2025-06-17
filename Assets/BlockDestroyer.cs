5e36245548bbbfb96f581948861660f7316dd74e
Lazism2j <lazism2j@gmail.com>
2025-06-17 11:17:20 +0900

    [Feat] BlockDestroy Button

--- Assets/BlockDestroyer.cs
+++ Assets/BlockDestroyer.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class BlockDestroyer : MonoBehaviour
 {
+    [SerializeField] int _radius;
+    [SerializeField] LayerMask Block;
+
+    /*
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Block")
         {
             Destroy(other.gameObject);
         }
+    }
+    */
+
+    private void FixedUpdate()
+    {
+        transform.localScale = Vector3.one * _radius;
+    }
+
+    public void BlockDestroy()
+    {
+        Collider[] inBlocks = Physics.OverlapSphere(transform.position, _radius, Block);
+        for (int i = 0; i < inBlocks.Length; i++)
+        {
+            Destroy(inBlocks[i].gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, _radius);
     }
 }
